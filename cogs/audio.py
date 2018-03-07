b0a99e0e19a762f8a6157c078c25596e3cbdb93c
Vitung Quach <vitungquach1494@gmail.com>
2018-03-06 21:34:53 -0500

    Refactor Audio cog and add !queue command.

--- cogs/audio.py
+++ cogs/audio.py
@@ -7,9 +7,10 @@ import asyncio
 "Adapted from https://github.com/Rapptz/discord.py/blob/async/examples/playlist.py"
 
 class AudioRequest:
-    def __init__(self, requester, channel, player):
-        self.requester = requester
-        self.channel = channel
+    def __init__(self, ctx, url, player):
+        self.requester = ctx.message.author
+        self.channel = ctx.message.channel
+        self.url = url
         self.player = player
 
     def __str__(self):
@@ -19,16 +20,54 @@ class AudioRequest:
    the handler will wait for one to arrive"""
 
 class PlaylistHandler:
-    def __init__(self, bot, voice=None, vol=0.2):
+    def __init__(self, bot):
         self.bot = bot
-        self.next_flag = asyncio.Event()
-        self.playlist = asyncio.Queue()
-        self.voice = voice
+        self.voice = None
         self.current = None
-        self.vol = vol   # The default volume that is used in all songs
+        self.next_flag = asyncio.Event()
+        self.playlist = asyncio.Queue() # Use this queue to process songs with nonblocking gets
+        self.playlist_printable = []    # Use this list to examine/mix queue contents with min disruption
+        self.vol = 0.2   # The default volume that is used in all songs
         self.playlist_task = self.bot.loop.create_task(self.process_playlist())
 
-    async def pause(self):
+    async def join(self, ctx, channel=None):
+        if channel is None:
+            channel = ctx.message.author.voice.voice_channel
+
+        if self.voice is None:
+            self.voice = await self.bot.join_voice_channel(channel)
+        else:
+            await self.voice.move_to(channel)
+
+        await self.bot.say("I am now in " + channel.name)
+
+    async def play(self, ctx, url):
+        """If bot not in channel, add bot to user's voice channel"""
+        if self.voice is None:
+            await self.join(ctx)
+
+        player = await self.voice.create_ytdl_player(url, after=self.play_next)
+        req = AudioRequest(ctx, url, player)
+        await self.playlist.put(req)
+        self.playlist_printable.append(req)
+
+        await self.bot.say("Added " + str(req) + " to the queue")
+
+    async def stop(self, ctx):
+        channel = self.voice.channel
+
+        """Cancel playlist processing and disconnect the voice client"""
+        self.playlist_task.cancel()
+        await self.voice.disconnect()
+        self.voice = None
+
+        """Reset the playlist"""
+        del self.playlist
+        self.playlist = asyncio.Queue()
+
+        await self.bot.say("I am no longer in " + channel.name)
+
+    async def pause(self, ctx):
         if self.not_playing():
             await self.bot.say("I am not currently playing any song!")
             return
@@ -36,7 +75,7 @@ class PlaylistHandler:
         self.current.player.pause()
         await self.bot.say("Paused " + str(self.current))
 
-    async def resume(self):
+    async def resume(self, ctx):
         if self.not_playing():
             await self.bot.say("I am not currently playing any song!")
             return
@@ -44,10 +83,21 @@ class PlaylistHandler:
         self.current.player.resume()
         await self.bot.say("Resumed " + str(self.current))
 
-    async def volume(self, vol):
-        self.vol = vol
+    async def volume(self, ctx, vol):
+        self.vol = float(vol) / 100
         if self.current is not None:
             self.current.player.volume = self.vol
+
+        await self.bot.say("Set the volume to {}%".format(vol))
+
+    async def queue(self, ctx):
+        queue_str = ""
+        num_iter = min(10, len(self.playlist_printable))
+        for i in range(num_iter):
+            queue_str += "**{}. {}**\n".format(i + 1, self.playlist_printable[i])
+
+        em = discord.Embed(title="Poppy's Playlist", description=queue_str, color=discord.Color.dark_purple())
+        await self.bot.say(embed=em)
 
     def not_playing(self):
         return self.voice is None or \
@@ -64,6 +114,7 @@ class PlaylistHandler:
             self.current.player.volume = self.vol
             await self.bot.send_message(self.current.channel, "Now playing " + str(self.current))
             self.current.player.start()
+            del self.playlist_printable[0]
             await self.next_flag.wait()
 
 
@@ -71,85 +122,53 @@ class AudioCog:
     def __init__(self, bot):
         self.bot = bot
         self.handlers = {}
-        self.vols = {}
 
     """TODO"""
-    async def _get_handler(self, server):
-        pass
-
-    async def _join_channel(self, channel=None):
-        """Creates a PlaylistHandler for the channel"""
-        id = channel.server.id
-        vol = self.vols.get(id, 1.0)
-        voice = await self.bot.join_voice_channel(channel)
-
-        self.handlers[channel.server.id] = PlaylistHandler(self.bot, voice, vol)
-        await self.bot.say("I am now in " + channel.name)
+    def _get_handler(self, server):
+        if server.id in self.handlers:
+            return self.handlers[server.id]
+        else:
+            handler = PlaylistHandler(self.bot)
+            self.handlers[server.id] = handler
+            return handler
 
     @commands.command(pass_context=True)
     async def join(self, ctx, channel: discord.Channel=None):
-        server = ctx.message.server
-
-        if channel is None:
-            channel = ctx.message.author.voice.voice_channel
-
-        if server.id in self.handlers:
-            voice = self.handlers[server.id].voice
-            await voice.move_to(channel)
-            await self.bot.say("I have moved to " + channel.name)
-        else:
-            await self._join_channel(channel)
+        handler = self._get_handler(ctx.message.server)
+        await handler.join(ctx, channel)
 
     @is_in_voice_channel()
     @commands.command(aliases=["leave"], pass_context=True)
     async def stop(self, ctx):
-        server = ctx.message.server
-        handler = self.handlers.pop(server.id)
-        channel = handler.voice.channel
-
-        handler.playlist_task.cancel()
-        await handler.voice.disconnect()
-        await self.bot.say("I am no longer in " + channel.name)
+        handler = self._get_handler(ctx.message.server)
+        await handler.stop(ctx)
 
     @commands.command(pass_context=True)
     async def play(self, ctx, url):
-        server = ctx.message.server
-
-        """If bot not in channel, add bot to user's voice channel"""
-        """TODO: Refactor this later on to make the logic cleaner"""
-        if server.id not in self.handlers or self.handlers[server.id].voice is None:
-            channel = ctx.message.author.voice.voice_channel
-            await self._join_channel(channel)
-
-        handler = self.handlers[server.id]
-        player = await handler.voice.create_ytdl_player(url, after=handler.play_next)
-        req = AudioRequest(ctx.message.author, ctx.message.channel, player)
-
-        await self.bot.say("Added " + str(req) + " to the queue")
-        await handler.playlist.put(req)
+        handler = self._get_handler(ctx.message.server)
+        await handler.play(ctx, url)
 
     @is_in_voice_channel()
     @commands.command(pass_context=True)
     async def pause(self, ctx):
-        handler = self.handlers[ctx.message.server.id]
-        await handler.pause()
+        handler = self._get_handler(ctx.message.server)
+        await handler.pause(ctx)
 
     @is_in_voice_channel()
     @commands.command(pass_context=True)
     async def resume(self, ctx):
-        handler = self.handlers[ctx.message.server.id]
-        await handler.resume()
+        handler = self._get_handler(ctx.message.server)
+        await handler.resume(ctx)
 
     @commands.command(pass_context=True)
     async def volume(self, ctx, vol):
-        id = ctx.message.server.id
-        self.vols[id] = float(vol)/100
+        handler = self._get_handler(ctx.message.server)
+        await handler.volume(ctx, vol)
 
-        if id in self.handlers:
-            handler = self.handlers[id]
-            await handler.volume(self.vols[id])
-
-        await self.bot.say("Set the volume to {}%".format(vol))
+    @commands.command(pass_context=True)
+    async def queue(self, ctx):
+        handler = self._get_handler(ctx.message.server)
+        await handler.queue(ctx)
 
 
 def setup(bot):
